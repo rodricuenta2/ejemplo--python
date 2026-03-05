69705cacdeae18dde7aca37260a85d700deb8bcd
rodricuenta2 <rodrigo.estroz+test@gmail.com>
2026-03-05 01:08:29 +0000

    viendo variables y tipos de datos en python

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+print("hola mundo que tal!")
+
+# variables:
+
+nombre = "rodrigo"
+edad = 47
+altura = 1.70
+es_profesor = False
+ciudad = "valdivia"
+genero = "masculino"
+
+
+
+#Tipos de Datos:
+
+str # esto es un texto
+int # esto es un entero
+float # esto es un flotante o con corte decimal
+bool # esto es verdadero o falso
+
+#INPUT
+#input()
+
+
+texto = input()
+print(texto)
+
+
