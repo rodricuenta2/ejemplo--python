4f5f6f2bdf335862254871f135e1fb6b0d10a44c
rodricuenta2 <rodrigo.estroz+test@gmail.com>
2026-03-10 00:56:57 +0000

    tipos de datos con ejercicios

--- main.py
+++ main.py
@@ -24,7 +24,41 @@ bool # esto es verdadero o falso
 #input()
 
 
-texto = input()
+texto = input("escribe tu nombre")
 print(texto)
+
+#numero = int (input ("escribe un numero :"))
+#resultado = numero +1
+#print(resultado)
+
+#INPUT
+precio =input(" escribe un precio") # pedir por consola un dato tipo
+precio = int(precio) # convertir a entero el str con int(precio)
+precio = precio + 10 #el precio sumado + 10 ya que ahora es numero entero
+print(precio) #imprime el precio en pantalla
+
+#CONDICIONALES
+
+# if condicion
+# codigo
+
+edad = 20
+
+if edad >= 18:
+    print ("puede entrar a la disco")
+else:
+    print ("no puede entrar a la disco")
+
+    #BUCLES
+
+    #WHILE
+    #while condicion:
+    #       codigo
+
+    numero = 0
+
+    while numero < 10:
+        print(numero)
+
 
 
---
+++ tiposdedatos.py
@@ -0,0 +1,41 @@
+## TIPOS DE DATOS
+
+#listas [ ]
+#diccionario { }
+#sets { }
+#tuplas ( )
+
+
+
+frutas = ["manzana","uva","kiwi","sandia"]
+# index      0        1      2      3
+
+#print(frutas[0])
+#print(frutas[1])
+#print(frutas[2])
+#print(frutas[3])
+
+
+frutas = ["manzana","uva","kiwi","sandia",[1,2,3,4,[5,6,7]]]
+# index       0       1     2       3          4              = posicion
+
+#print(frutas[4][4][0])
+
+#print(dir(frutas))
+
+
+# append es una funcion y agrega elementos al final de la lista
+
+frutas.append("naranja")
+print(frutas)
+
+
+#remove quita un elemento de la lista
+
+
+personas = ["cristian","esteban"]
+
+#for i in personas:
+ #   print(i)
+
+ 
